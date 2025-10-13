c38fb3d1bfe38846fe39f05771916e56ce46fca0
SadmanMuhit <sadmanmuhit@gmail.com>
2025-10-14 01:25:29 +0600

    upgrade banner

--- src/Componets/Home/Banner.jsx
+++ src/Componets/Home/Banner.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Banner = () => {
   return (
     <>
-    <section className='bg-[url("/banner.png")]'>
-      <div className='container m-auto pt-[153px] pb-[263px] flex flex-wrap justify-end'>
-      <div className='bg-[#FFF3E3] w-fit pt-[62px] pl-10 pr-11 pb-9 rounded-[10px]'>
+    <section className='bg-[url("/banner.png")] bg-no-repeat bg-center bg-cover'>
+      <div className='container m-auto pt-[500px] pb-[263px] flex justify-end'>
+      <div className='bg-[#FFF3E3] pt-[62px] pl-10 pr-11 pb-9 rounded-[10px]'>
       <h5 className='text-base font-semibold font-poppins text-[#333333] tracking-[3px]'>New Arrival</h5>
       <h1 className='text-[52px] font-bold font-poppins leading-16 text-[#B88E2F] w-[600px] mt-1 mb-4'>Discover Our New Collection</h1>
       <p className='text-lg font-medium font-poppins text-[#333333] w-[546px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
--- src/Componets/Home/Product.jsx
+++ src/Componets/Home/Product.jsx
@@ -28,7 +28,7 @@ const Product = () => {
                     </div>
                 </div>
                 <div>
-                    <img src="/product2     .png" alt="" className='w-full h-60 object-cover'/>
+                    <img src="/product2.png" alt="" className='w-full h-60 object-cover'/>
                     <div className='bg-[#F4F5F7] pl-4 pt-4 pb-[30px]'>
                         <h3 className='text-2xl font-semibold font-poppins text-[#3A3A3A]'>Syltherine</h3>
                         <p className='text-base font-medium font-poppins text-[#898989] pt-2'>Stylish cafe chair</p>
