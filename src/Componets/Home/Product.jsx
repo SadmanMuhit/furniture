21dde211aeb1dfd728f11620e5fb4919532356e5
SadmanMuhit <ssmuhit9699@gmail.com>
2025-12-01 01:10:28 +0600

    button

--- src/Componets/Home/Product.jsx
+++ src/Componets/Home/Product.jsx
@@ -96,6 +96,7 @@ const Product = () => {
         <div className='flex justify-center mt-8'>
             <button className='border py-3 px-[74px] border-[#B88E2F] text-base font-semibold font-poppins text-[#B88E2F]'>Show More</button>
         </div>
+        <button>signup</button>
         </div>
     </section>
   )
